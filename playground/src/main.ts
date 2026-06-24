7a6990e90f176c93940a0555921a70a16957d43e
Panpan <wurui.dev@gmail.com>
2026-06-24 15:07:46 +0800

    chore: add @types/node to playground, add tsconfig exclude

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,6 +1,7 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 
+// eslint-disable-next-line ts/explicit-function-return-type
 export function createApp() {
   const app = createSSRApp(App)
   return {
